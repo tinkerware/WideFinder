e7880984e9a381fed32d6b3de3175b4e0b2ee7dd
Evgeny Goldin <evgenyg@gmail.com>
2010-03-30 19:28:16 +0300

    MapsTest commit

---
+++ src/test/groovy/widefinder/maps/MapsTest.groovy
@@ -0,0 +1,34 @@
+
+package widefinder.maps
+
+import org.junit.Test
+
+
+class MapsTest extends GroovyTestCase
+{
+
+    @Test
+    void test1()
+    {
+        Map m = new HashMap<String, String>( 1234 )
+
+        1.upto( 10000 ){ int j -> m[ "[$j]" ] = "<$j>" }
+        assertEquals( 10000, m.size())
+
+        10000.downto( 1 ){ int j -> assertEquals( "<$j>", m[ "[$j]" ]    ) }
+        10000.downto( 1 ){ int j -> assertEquals( "<$j>", m.get( "[$j]" )) }
+    }
+
+
+    @Test
+    void test2()
+    {
+        Map m = new HashMap<String, String>( 1234 )
+
+        1.upto( 10000 ){ int j -> m[ "[$j]" ] = "<$j>" }
+        assertEquals( 10000, m.size())
+
+        10000.downto( 1 ){ int j -> assertEquals( "<$j>", m[ "[$j]" ]              ) }
+        10000.downto( 1 ){ int j -> assertEquals( "<$j>", m.get( "[$j]".toString())) }
+    }
+}
